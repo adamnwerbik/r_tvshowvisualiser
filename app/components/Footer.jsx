c8837396fe910462489c19fb268fa42f28492727
Adam Nicholas Werbik <69626130+adamnwerbik@users.noreply.github.com>
2024-02-12 03:57:15 +1100

    Update Footer.jsx

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 const Footer = () => {
   return (
-    <footer className="flex flex-col items-center fixed bottom-0 left-0 z-20 w-full p-3 bg-white border-t border-gray-200 shadow ">
+    <footer className="flex flex-col items-center fixed bottom-0 left-0 z-20 w-full p-3 bg-white border-t border-gray-200 shadow text-balance text-center ">
       <div className="italic">
         made by{" "}
         <Link href={"https://github.com/adamnwerbik"}>💻 adamnwerbik 🔥</Link>{" "}

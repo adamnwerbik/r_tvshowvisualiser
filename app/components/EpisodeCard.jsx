b151a5758d30e721fa35a7485b226e60d3994c9c
Adam Nicholas Werbik <69626130+adamnwerbik@users.noreply.github.com>
2024-02-14 16:36:37 +1100

    only show image if supplied

--- app/components/EpisodeCard.jsx
+++ app/components/EpisodeCard.jsx
@@ -3,11 +3,15 @@ import React from "react";
 const EpisodeCard = (props) => {
   return (
     <div className="flex flex-col items-center p-2 m-5 bg-white border border-gray-200 rounded-lg shadow md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-      <img
-        class="object-cover w-full rounded-lg md:mid-w-48 h-36"
-        src={`https://image.tmdb.org/t/p/w500/${props.epObj.still_path}`}
-        alt="AA"
-      />
+      {props.epObj.still_path ? (
+        <img
+          class="object-cover w-full rounded-lg md:mid-w-48 h-36"
+          src={`https://image.tmdb.org/t/p/w500/${props.epObj.still_path}`}
+          alt="Episode Thumbnail Image"
+        />
+      ) : (
+        ""
+      )}
       <div class="flex flex-col justify-between p-4 leading-normal">
         <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
           {props.epObj.name}

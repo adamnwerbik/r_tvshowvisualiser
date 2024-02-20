8dadba01a1e6794585dcff373c96937dd4378527
Adam Nicholas Werbik <69626130+adamnwerbik@users.noreply.github.com>
2024-02-20 22:06:53 +1100

    minor change to phone styling

--- app/components/showCase.jsx/Video.jsx
+++ app/components/showCase.jsx/Video.jsx
@@ -5,7 +5,7 @@ export function Video() {
       autoPlay
       muted
       preload="none"
-      className="border-gray-200 border mt-36 md:mt-24 rounded-lg w-5/6 md:w-3/5 lg:w-2/5 lg:mt-6 xl:w-3/12 xl:mt-32 "
+      className="border-gray-200 border mt-24 md:mt-24 rounded-lg w-5/6 md:w-3/5 lg:w-2/5 lg:mt-6 xl:w-3/12 xl:mt-32 "
     >
       <source src="./TVVisShowcase2.webm" type="video/webm" />
       <source src="./TVVisShowcase2.mp4" type="video/mp4" />
